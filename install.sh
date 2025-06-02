2c9e7a2e4df3f41270ff7697a22677642184212c
t.tsukada <122663114+Taro20011815@users.noreply.github.com>
2025-06-02 17:54:29 +0900

    Fix error  of language japanese

--- install.sh
+++ install.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 echo "╔══╣ Install: kokoro_tts_ros (STARTING) ╠══╗"
 
+set -e  # 途中で失敗したら即終了
+
+# apt の自動確認スキップ
+export DEBIAN_FRONTEND=noninteractive
+
 sudo apt update -y
 
-sudo apt install -y ros-humble-vision-msgs
+sudo apt install -y ros-humble-vision-msgs espeak-ng
 
-pip3 install soundfile
+# Python パッケージのインストールと整備
+pip3 install -q soundfile pygame
 
-pip3 install pygame
+# kokoro および misaki TTS ライブラリ
+pip3 install -q kokoro==0.9.4
+pip3 install -q 'misaki[en,ja]==0.9.4'
 
-sudo apt install espeak-ng
+pip3 uninstall -y fugashi unidic unidic-lite
+pip3 install -q fugashi unidic-lite
 
-pip3 install kokoro==0.9.4
-pip3 install misaki[en,ja]==0.9.4
-
+# ROS パッケージのクローン
 cd ~/colcon_ws/src/
-
-git clone -b humble-devel https://github.com/TeamSOBITS/sobits_msgs.git
+if [ ! -d "sobits_msgs" ]; then
+    git clone -b humble-devel https://github.com/TeamSOBITS/sobits_msgs.git
+else
+    echo "sobits_msgs リポジトリはすでに存在します。スキップします。"
+fi
 
 echo "╚══╣ Install: kokoro_tts_ros (FINISHED) ╠══╝"
